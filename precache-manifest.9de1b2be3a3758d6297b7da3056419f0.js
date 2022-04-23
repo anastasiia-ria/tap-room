931a41ffb28211f6f71b66b383ca7977ea7a541a
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-04-22 17:09:47 -0700

    Updates

---
+++ precache-manifest.9de1b2be3a3758d6297b7da3056419f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e589a53374988d58cc04451b32e35b4d",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "a8f7ac78e7b326808937",
+    "url": "/tap-room/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "8a6a9e1ebe017a3defc1",
+    "url": "/tap-room/static/css/main.ce86de99.chunk.css"
+  },
+  {
+    "revision": "a8f7ac78e7b326808937",
+    "url": "/tap-room/static/js/2.8e480f1a.chunk.js"
+  },
+  {
+    "revision": "85f1b5c9f5dadf2445a8",
+    "url": "/tap-room/static/js/3.19d4de79.chunk.js"
+  },
+  {
+    "revision": "8a6a9e1ebe017a3defc1",
+    "url": "/tap-room/static/js/main.0abcb187.chunk.js"
+  },
+  {
+    "revision": "aad7e8999c75586d6c0d",
+    "url": "/tap-room/static/js/runtime-main.cfe986ab.js"
+  }
+]);
